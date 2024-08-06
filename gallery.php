e666fb19a9249e4a5066e71be12bb7c940431c01
gayatri <varshadkale188@gmail.com>
2024-08-06 18:51:11 +0530

    minor changes

--- gallery.php
+++ gallery.php
@@ -1,30 +1,6 @@
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Be aHand Charity Foundation, Fundraise And Donation HTML Template" />
-	<meta name="keywords" content="Causes, charity, charity agency, charity html template, church, donation, food, foundation, help, ngo, non-profit organization, organization, social, volunteer, welfare">
-	<meta name="author" content="unlockdesign">
-	<!-- css file -->
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/all-plugins.css">
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="css/theme-color.css">
-	<!-- Responsive stylesheet -->
-	<link rel="stylesheet" href="css/responsive.css">
-	<!-- Title -->
-	<title>Be aHand Charity Foundation, Fundraise And Donation HTML Template</title>
-	<!-- Favicon -->
-	<link href="images/favicon.ico" sizes="128x128" rel="shortcut icon" type="image/x-icon" />
-	<link href="images/favicon.ico" sizes="128x128" rel="shortcut icon" />
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
+<?php
+include 'header.php';
+?>
 
 <body>
 	<div class="wrapper">
@@ -1377,4 +1353,6 @@
 			</div>
 		</section>
 
-		
+		<?php
+include 'footer.php';
+?>
