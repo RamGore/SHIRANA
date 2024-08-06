017f5e48e9adc16f27d238f930eaae1f76e13605
gayatri <varshadkale188@gmail.com>
2024-08-06 11:32:38 +0530

    created header and footer pages

---
+++ footer.php
@@ -0,0 +1,87 @@
+<!-- Our Footer -->
+<section class="ulockd-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-3 col-lg-4">
+					<div class="ulockd-footer-fst-col">
+						<img alt="" src="images/footer-logo.png" class="img-responsive ulockd-footer-log">
+    					<p class="ulockd-ftr-text">Regardless of whether you need to stay in your own house, are searching for help with a more established relative, looking for exhortation on paying for care, we can help you.</p>
+    					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam quis assumenda adipisci, quasi perferendis id.</p>
+						<ul class="list-inline ulockd-footer-font-icon">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-feed"></i></a></li>
+							<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+							<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+							<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-3 col-lg-2">
+					<div class="ulockd-footer-qlink">
+						<h3 class="text-uppercase">NAVIGATI<span class="text-thm2">ON</span></h3>
+						<ul class="list-unstyled">
+							<li><a href="#">About Us</a></li>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">News</a></li>
+							<li><a href="#">Our Services</a></li>
+							<li><a href="#">Sitemap</a></li>
+							<li><a href="#">Team Details</a></li>
+							<li><a href="#">Contact Us</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+					<div class="ulockd-footer-contact">
+						<h3 class="text-uppercase">CONTACT <span class="text-thm2">US</span></h3>
+						<p>Victoria 8007 Australia Envato HQ 121 King Street, Melbourne.</p>
+						<div class="ulockd-ftr-phone"><span>Phone </span>: <a href="#"> 99 55 66 88 526</a></div>
+						<div class="ulockd-ftr-fax"><span>Fax </span>: <a href="#"> 55 44 77 62</a></div>
+						<div class="ulockd-ftr-mail"><span>Email </span>: <a href="#"> beahand@email.com</a></div>
+						<div class="ulockd-ftr-mail"><span>Website </span>: <a href="#"> https://envato.com/</a></div>
+						<div class="ulockd-ftr-mail"><span>Website </span>: <a href="#"> https://themeforest.net/</a></div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+					<div class="ulockd-footer-twitter example1">
+						<h3 class="text-uppercase">TWITT<span class="text-thm2">ER</span></h3>
+						<div class="twitter"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Our CopyRight Section -->
+	<section class="ulockd-copy-right">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<p class="color-white">Copyright© Be aHand. <a class="color-white" href="https://goo.gl/8HdP67" target="_blank">UnlockDesign</a> All right reserved.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+<a class="scrollToHome" href="#"><i class="fa fa-home"></i></a>
+</div>
+<!-- Wrapper End -->
+<script type="text/javascript" src="js/jquery-1.12.4.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/bootsnav.js"></script>
+<script type="text/javascript" src="js/parallax.js"></script>
+<script type="text/javascript" src="js/scrollto.js"></script>
+<script type="text/javascript" src="js/jquery-scrolltofixed-min.js"></script>
+<script type="text/javascript" src="js/jquery.counterup.js"></script>
+<script type="text/javascript" src="js/gallery.js"></script>
+<script type="text/javascript" src="js/wow.min.js"></script>
+<script type="text/javascript" src="js/slider.js"></script>
+<script type="text/javascript" src="js/video-player.js"></script>
+<script type="text/javascript" src="js/jquery.barfiller.js"></script>
+<script type="text/javascript" src="js/timepicker.js"></script>
+<script type="text/javascript" src="js/tweetie.js"></script>
+<script type="text/javascript" src="vendor/s-customizer/js/color-switcher.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+</body>
+
+<!-- Mirrored from unlockdesizn.com/html/nonprofit/be-ahand/page-about.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 30 Jul 2024 05:33:29 GMT -->
+</html>
