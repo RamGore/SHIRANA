c68aeeeaffcef04ca55e007593af6ed91fbc3248
Ram Gore <gore64643@gmail.com>
2024-08-07 17:55:32 +0530

    some changes in home

--- footer.php
+++ footer.php
@@ -1,66 +1,73 @@
 <!-- Our Footer -->
 <section class="ulockd-footer">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12 col-sm-6 col-md-3 col-lg-4">
-					<div class="ulockd-footer-fst-col">
-						<img alt="" src="images/footer-logo.png" class="img-responsive ulockd-footer-log">
-    					<p class="ulockd-ftr-text">Regardless of whether you need to stay in your own house, are searching for help with a more established relative, looking for exhortation on paying for care, we can help you.</p>
-    					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam quis assumenda adipisci, quasi perferendis id.</p>
-						<ul class="list-inline ulockd-footer-font-icon">
-							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-							<li><a href="#"><i class="fa fa-feed"></i></a></li>
-							<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-							<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-							<li><a href="#"><i class="fa fa-instagram"></i></a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-xs-12 col-sm-6 col-md-3 col-lg-2">
-					<div class="ulockd-footer-qlink">
-						<h3 class="text-uppercase">NAVIGATI<span class="text-thm2">ON</span></h3>
-						<ul class="list-unstyled">
-							<li><a href="#">About Us</a></li>
-							<li><a href="#">Home</a></li>
-							<li><a href="#">News</a></li>
-							<li><a href="#">Our Services</a></li>
-							<li><a href="#">Sitemap</a></li>
-							<li><a href="#">Team Details</a></li>
-							<li><a href="#">Contact Us</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-					<div class="ulockd-footer-contact">
-						<h3 class="text-uppercase">CONTACT <span class="text-thm2">US</span></h3>
-						<p>Victoria 8007 Australia Envato HQ 121 King Street, Melbourne.</p>
-						<div class="ulockd-ftr-phone"><span>Phone </span>: <a href="#"> 99 55 66 88 526</a></div>
-						<div class="ulockd-ftr-fax"><span>Fax </span>: <a href="#"> 55 44 77 62</a></div>
-						<div class="ulockd-ftr-mail"><span>Email </span>: <a href="#"> beahand@email.com</a></div>
-						<div class="ulockd-ftr-mail"><span>Website </span>: <a href="#"> https://envato.com/</a></div>
-						<div class="ulockd-ftr-mail"><span>Website </span>: <a href="#"> https://themeforest.net/</a></div>
-					</div>
-				</div>
-				<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-					<div class="ulockd-footer-twitter example1">
-						<h3 class="text-uppercase">TWITT<span class="text-thm2">ER</span></h3>
-						<div class="twitter"></div>
-					</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-3 col-lg-4">
+				<div class="ulockd-footer-fst-col">
+						<div class="ulockd-welcm-hmddl">
+							<p href="index.html" class="ulockd-main-logo main-logo">NAGARI VIKAS<br>
+								SEVABHAVI SANSTHA</p>
+						</div>
+					<!-- <img alt="" src="images/footer-logo.png" class="img-responsive ulockd-footer-log"> -->
+					<p class="ulockd-ftr-text">Regardless of whether you need to stay in your own house, are searching for help with a more established relative, looking for exhortation on paying for care, we can help you.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam quis assumenda adipisci, quasi perferendis id.</p>
 				</div>
 			</div>
-		</div>
-	</section>
+			<div class="col-xs-12 col-sm-6 col-md-3 col-lg-2">
+				<div class="ulockd-footer-qlink">
+					<h3 class="text-uppercase">NAVIGATI<span class="text-thm2">ON</span></h3>
+					<ul class="list-unstyled">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="about.php">About Us</a></li>
+						<li><a href="activity.php">Our Activity</a></li>
+						<li><a href="#">Events</a></li>
+						<li><a href="gallery.php">Gallery</a></li>
+						<li><a href="contact-us.php">Contact Us</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+				<div class="ulockd-footer-contact">
+					<h3 class="text-uppercase">CONTACT <span class="text-thm2">US</span></h3>
+					<p>Victoria 8007 Australia Envato HQ 121 King Street, Melbourne.</p>
+					<div class="ulockd-ftr-phone"><span>Phone </span>: <a href="tel:+91 8308136689" class="ulockd-addrss">+91 8308136689,</a><a href="tel:+91 8668615130" class="ulockd-addrss">+91 8668615130</a>
+					</div>
+					<div class="ulockd-ftr-fax"><span>Fax </span>: <a href="#"> 55 44 77 62</a></div>
+					<div class="ulockd-ftr-mail"><span>Email </span>: <a href="mailto:lalchandrajput1967@gmail.com">lalchandrajput1967@gmail.com , </a><a href="mailto:sachintatu0009@gmail.com"> sachintatu0009@gmail.com</a>
+					</div>
+					<!-- <div class="ulockd-ftr-mail"><span>Website </span>: <a href="#"> https://envato.com/</a></div>
+					<div class="ulockd-ftr-mail"><span>Website </span>: <a href="#"> https://themeforest.net/</a></div> -->
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+				<div class="ulockd-footer-twitter example1">
+					<h3 class="text-uppercase">FOLLOW <span class="text-thm2">US</span></h3>
 
-	<!-- Our CopyRight Section -->
-	<section class="ulockd-copy-right">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<p class="color-white">Copyright© Be aHand. <a class="color-white" href="https://goo.gl/8HdP67" target="_blank">UnlockDesign</a> All right reserved.</p>
+					
+					<ul class="list-inline ulockd-footer-font-icon">
+						<li><a href="#"><i class="fa-brands fa-facebook-f" style="color: #fff;"></i></a></li>
+						<li><a href="#"><i class="fa-brands fa-whatsapp" style="color: #fff;"></i></a></li>
+						<li><a href="#"><i class="fa-brands fa-instagram" style="color: #fff;"></i></a></li>
+					</ul>
+					
 				</div>
 			</div>
 		</div>
-	</section>
+	</div>
+</section>
+
+<!-- Our CopyRight Section -->
+<section class="ulockd-copy-right">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 cont-cent">
+				<p class="color-white">Copyright © 2024.All right reserved. </p>
+				<span class="color-white">Designed and Developed by<a class="color-white" href="https://websartech.com/" target="_blank">Websar IT Solutions</a></span>
+
+			</div>
+		</div>
+	</div>
+</section>
 
 <a class="scrollToHome" href="#"><i class="fa fa-home"></i></a>
 </div>
@@ -83,4 +90,5 @@
 </body>
 
 <!-- Mirrored from unlockdesizn.com/html/nonprofit/be-ahand/page-about.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 30 Jul 2024 05:33:29 GMT -->
+
 </html>
