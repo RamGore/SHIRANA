fee13bab2376d4ce5f0570c685efe66eb91da193
Ram Gore <gore64643@gmail.com>
2024-08-08 12:22:22 +0530

    mainar changes

--- contact-us.php
+++ contact-us.php
@@ -1,6 +1,7 @@
 <?php
 include 'header.php';
 ?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
 <div class="wrapper">
 	<div id="preloader" class="preloader">
