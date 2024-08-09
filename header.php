b658d04c7aa1cf7422e98719dd3efe77183143ba
gayatri <varshadkale188@gmail.com>
2024-08-09 18:48:42 +0530

    improve header file

--- header.php
+++ header.php
@@ -8,12 +8,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Be aHand Charity Foundation, Fundraise And Donation HTML Template" />
-<<<<<<< HEAD
-	<meta name="keywords"
-		content="Causes, charity, charity agency, charity html template, church, donation, food, foundation, help, ngo, non-profit organization, organization, social, volunteer, welfare">
-=======
 	<meta name="keywords" content="Causes, charity, charity agency, charity html template, church, donation, food, foundation, help, ngo, non-profit organization, organization, social, volunteer, welfare">
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 	<meta name="author" content="unlockdesign">
 	<!-- css file -->
 	<link rel="stylesheet" href="css/bootstrap.min.css">
@@ -35,15 +30,9 @@
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
-<<<<<<< HEAD
-	  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-=======
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 </head>
 
 <body>
@@ -71,23 +60,6 @@
 		<div class="header-middle">
 			<div class="container">
 				<div class="row center">
-<<<<<<< HEAD
-					<div class="col-xs-12 col-sm-6 col-lg-3 col-lg-3">
-						<div class="ulockd-welcm-hmddl">
-							<a href="index.html" class="ulockd-main-logo main-logo">NAGARI VIKAS<br>
-								SEVABHAVI SANSTHA</a>
-						</div>
-					</div>
-					<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-						<div class="ulockd-ohour-info style2">
-							<div class="ulockd-icon pull-left text-thm2"><span
-									class="flaticon-email-filled-closed-envelope"></span></div>
-							<div class="ulockd-info">
-								<h3>Mail Us</h3>
-								<a href="mailto:sachintatu0009@gmail.com"> sachintatu0009@gmail.com</a>
-
-
-=======
 					<div class="col-xs-12 col-sm-6 col-lg-3">
 						<div class="ulockd-welcm-hmddl">
 							<a href="index.php"  class="header-logo ulockd-main-logo main-logo">
@@ -103,18 +75,12 @@
 							<div class="ulockd-info">
 								<h3>Mail Us</h3>
 								<a href="mailto:sachintatu0009@gmail.com"> sachintatu0009@gmail.com</a>
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 							</div>
 						</div>
 					</div>
 					<div class="col-xs-12 col-sm-6 col-lg-3 col-lg-3">
 						<div class="ulockd-ohour-info style2">
-<<<<<<< HEAD
-							<div class="ulockd-icon pull-left text-thm2"><span class="flaticon-old-handphone"></span>
-							</div>
-=======
 							<div class="ulockd-icon pull-left text-thm2"><span class="flaticon-old-handphone"></span></div>
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 							<div class="ulockd-info">
 								<h3>Call Us</h3>
 								<a href="tel:+91 8308136689" class="ulockd-addrss">+91 8308136689</a>
@@ -124,35 +90,18 @@
 					</div>
 					<div class="col-xs-12 col-sm-6 col-lg-3 col-lg-3">
 						<div class="ulockd-ohour-info style2">
-<<<<<<< HEAD
-							<div class="ulockd-icon pull-left text-thm2"><span class="flaticon-make-a-donation"></span>
-							</div>
-							<div class="ulockd-info">
-								<!-- Trigger the modal with a button -->
-								<button type="button" class="btn btn-lg ulockd-btn-thm2 ulockd-mrgn1215 ulockd-mrgn610"
-									data-toggle="modal" data-target=".bs-example-modal-default">Donate now</button>
-								<!-- Modal -->
-								<div id="Donation-form-modal" class="modal fade bs-example-modal-default text-left"
-									role="dialog">
-=======
 							<div class="ulockd-icon pull-left text-thm2"><span class="flaticon-make-a-donation"></span></div>
 							<div class="ulockd-info">
 								<!-- Trigger the modal with a button -->
 								<button type="button" class="btn btn-lg ulockd-btn-thm2 ulockd-mrgn1215 ulockd-mrgn610" data-toggle="modal" data-target=".bs-example-modal-default">Donate now</button>
 								<!-- Modal -->
 								<div id="Donation-form-modal" class="modal fade bs-example-modal-default text-left" role="dialog">
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 									<div class="modal-dialog">
 
 										<!-- Modal content-->
 										<div class="modal-content">
 											<div class="modal-header text-center">
-<<<<<<< HEAD
-												<button type="button" class="close"
-													data-dismiss="modal">&times;</button>
-=======
 												<button type="button" class="close" data-dismiss="modal">&times;</button>
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 												<h3> <span class="text-thm2"></span> DONATION FORM</h3>
 												<p>Your Donation Can Save Lot's Of Life</p>
 											</div>
@@ -170,20 +119,6 @@
 																<label for="radio-two"> $20</label>
 															</li>
 															<li class="amount-box">
-<<<<<<< HEAD
-																<input id="radio-three" type="radio"
-																	name="payment-group">
-																<label for="radio-three"> $30</label>
-															</li>
-															<li class="amount-box">
-																<input id="radio-four" type="radio"
-																	name="payment-group">
-																<label for="radio-four"> $50</label>
-															</li>
-															<li class="amount-box">
-																<input id="radio-five" type="radio"
-																	name="payment-group">
-=======
 																<input id="radio-three" type="radio" name="payment-group">
 																<label for="radio-three"> $30</label>
 															</li>
@@ -193,7 +128,6 @@
 															</li>
 															<li class="amount-box">
 																<input id="radio-five" type="radio" name="payment-group">
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 																<label for="radio-five"> $100</label>
 															</li>
 														</ul>
@@ -217,19 +151,10 @@
 														<form class="form-inline">
 															<h4>Custom Amount</h4>
 															<div class="form-group">
-<<<<<<< HEAD
-																<label class="sr-only" for="exampleInputAmount">Amount
-																	(in dollars)</label>
-																<div class="input-group">
-																	<div class="input-group-addon">$</div>
-																	<input type="number" class="form-control"
-																		id="exampleInputAmount" placeholder="Amount">
-=======
 																<label class="sr-only" for="exampleInputAmount">Amount (in dollars)</label>
 																<div class="input-group">
 																	<div class="input-group-addon">$</div>
 																	<input type="number" class="form-control" id="exampleInputAmount" placeholder="Amount">
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 																	<div class="input-group-addon">.00</div>
 																</div>
 															</div>
@@ -241,123 +166,57 @@
 															<div class="row">
 																<div class="col-md-6">
 																	<div class="form-group">
-<<<<<<< HEAD
-																		<input type="text" class="form-control required"
-																			id="exampleInputNamex"
-																			placeholder="First Name">
-=======
 																		<input type="text" class="form-control required" id="exampleInputNamex" placeholder="First Name">
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 																	</div>
 																</div>
 																<div class="col-md-6">
 																	<div class="form-group">
-<<<<<<< HEAD
-																		<input type="text" class="form-control required"
-																			id="exampleInputNamexx"
-																			placeholder="Last Name">
-=======
 																		<input type="text" class="form-control required" id="exampleInputNamexx" placeholder="Last Name">
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 																	</div>
 																</div>
 																<div class="col-md-6">
 																	<div class="form-group">
-<<<<<<< HEAD
-																		<input type="email"
-																			class="form-control required"
-																			id="exampleInputEmailxy"
-																			placeholder="Email">
-=======
 																		<input type="email" class="form-control required" id="exampleInputEmailxy" placeholder="Email">
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 																	</div>
 																</div>
 																<div class="col-md-6">
 																	<div class="form-group">
-<<<<<<< HEAD
-																		<input type="text" class="form-control required"
-																			id="exampleInputPhone" placeholder="Phone">
-=======
 																		<input type="text" class="form-control required" id="exampleInputPhone" placeholder="Phone">
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 																	</div>
 																</div>
 																<div class="col-md-6">
 																	<div class="form-group">
-<<<<<<< HEAD
-																		<input type="text" class="form-control required"
-																			id="exampleInputAddress"
-																			placeholder="Address line 1">
-=======
 																		<input type="text" class="form-control required" id="exampleInputAddress" placeholder="Address line 1">
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 																	</div>
 																</div>
 																<div class="col-md-6">
 																	<div class="form-group">
-<<<<<<< HEAD
-																		<input type="text" class="form-control required"
-																			id="exampleInputAddress2"
-																			placeholder="Address line 2">
-=======
 																		<input type="text" class="form-control required" id="exampleInputAddress2" placeholder="Address line 2">
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 																	</div>
 																</div>
 																<div class="col-md-6">
 																	<div class="form-group">
-<<<<<<< HEAD
-																		<input type="text" class="form-control required"
-																			id="exampleInputCity"
-																			placeholder="City/State">
-=======
 																		<input type="text" class="form-control required" id="exampleInputCity" placeholder="City/State">
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 																	</div>
 																</div>
 																<div class="col-md-6">
 																	<div class="form-group">
-<<<<<<< HEAD
-																		<input type="text" class="form-control required"
-																			id="exampleInputZip"
-																			placeholder="Zipcode/Postcode">
-=======
 																		<input type="text" class="form-control required" id="exampleInputZip" placeholder="Zipcode/Postcode">
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 																	</div>
 																</div>
 															</div>
 															<div class="form-group">
-<<<<<<< HEAD
-																<textarea id="form_message" name="form_message"
-																	class="form-control required" rows="4"
-																	placeholder="Additional Note"></textarea>
-															</div>
-															<div class="form-group text-center">
-																<button type="submit"
-																	class="btn btn-lg btn-block ulockd-btn-thm2"
-																	data-toggle="modal"
-																	data-target=".bs-example-modal-default">Donate
-																	now</button>
-=======
 																<textarea id="form_message" name="form_message" class="form-control required" rows="4" placeholder="Additional Note"></textarea>
 															</div>
 															<div class="form-group text-center">
 																<button type="submit" class="btn btn-lg btn-block ulockd-btn-thm2" data-toggle="modal" data-target=".bs-example-modal-default">Donate now</button>
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 															</div>
 														</form>
 													</div>
 												</div>
 											</div>
 											<div class="modal-footer">
-<<<<<<< HEAD
-												<button type="button" class="btn btn-default"
-													data-dismiss="modal">Close</button>
-=======
 												<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 											</div>
 										</div>
 									</div>
@@ -372,11 +231,7 @@
 		<!-- Header Styles -->
 		<header class="header-nav">
 			<div class="main-header-nav navbar-scrolltofixed">
-<<<<<<< HEAD
-				<div class="container">
-=======
 				<div class="container back-colr">
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 					<nav class="navbar navbar-default bootsnav menu-style1">
 						<!-- Start Top Search -->
 						<div class="top-search">
@@ -394,12 +249,6 @@
 
 							<!-- Start Header Navigation -->
 							<div class="navbar-header">
-<<<<<<< HEAD
-								<button type="button" class="navbar-toggle" data-toggle="collapse"
-									data-target="#navbar-menu">
-									<i class="fa fa-bars"></i>
-								</button>
-=======
 								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
 									<i class="fa fa-bars"></i>
 								</button>
@@ -407,16 +256,11 @@
 									<p class="ulockd-welcntxt">SHIRANA WOMEN ENTERPENUAR DEVELOPMENT
 										CENTER</p>
 								</div>
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 							</div>
 							<!-- End Header Navigation -->
 
 							<!-- Collect the nav links, forms, and other content for toggling -->
-<<<<<<< HEAD
-							<!-- <div class="collapse navbar-collapse" id="navbar-menu">
-=======
 							<div class="collapse navbar-collapse" id="navbar-menu">
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 								<ul class="nav navbar-nav navbar-left">
 
 									<li class="dropdown">
@@ -429,11 +273,7 @@
 											<li><a href="doc-gallery">Document Gallery</a></li>
 										</ul>
 									</li>
-<<<<<<< HEAD
-									<li class="dropdown ">
-=======
 									<li class="dropdown">
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 										<a href="activity">Our Activities</a>
 									</li>
 									<li class="dropdown">
@@ -446,40 +286,7 @@
 										<a href="contact-us">Contact Us</a>
 									</li>
 								</ul>
-<<<<<<< HEAD
-							</div>/.navbar-collapse -->
-
-							<div class="collapse navbar-collapse" id="navbar-menu">
-    <ul class="nav navbar-nav navbar-left">
-        <li class="dropdown">
-            <a href="index">Home</a>
-        </li>
-        <li class="dropdown">
-            <a href="about" class="dropdown-toggle" data-toggle="dropdown">About Us</a>
-            <ul class="dropdown-menu animated" style="display: none; opacity: 1;">
-                <li><a href="org-profile">Organisation Profile</a></li>
-                <li><a href="doc-gallery">Document Gallery</a></li>
-            </ul>
-        </li>
-        <li class="dropdown">
-            <a href="activity">Our Activities</a>
-        </li>
-        <li class="dropdown">
-            <a href="event">Events</a>
-        </li>
-        <li class="dropdown">
-            <a href="gallery">Gallery</a>
-        </li>
-        <li class="dropdown">
-            <a href="contact-us">Contact Us</a>
-        </li>
-    </ul>
-</div>
-
-
-=======
 							</div><!-- /.navbar-collapse -->
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 						</div>
 
 						<!-- Start Side Menu -->
@@ -488,11 +295,7 @@
 							<div class="widget">
 								<h4 class="title">Custom Pages</h4>
 								<ul class="link">
-<<<<<<< HEAD
-									<li><a href="about">About</a></li>
-=======
 									<li><a href="#">About</a></li>
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 									<li><a href="#">Services</a></li>
 									<li><a href="#">Blog</a></li>
 									<li><a href="#">Portfolio</a></li>
@@ -507,13 +310,7 @@
 									<li><a href="#">Parallax Sections</a></li>
 									<li><a href="#">Shortcode Central</a></li>
 									<li><a href="#">Ultimate Font Collection</a></li>
-<<<<<<< HEAD
-									<li><img title="Facebook Feed With Client File"
-											class="img-responsive ulockd-mrgn1210" src="images/resource/image3.png"
-											alt="image3.png"></li>
-=======
 									<li><img title="Facebook Feed With Client File" class="img-responsive ulockd-mrgn1210" src="images/resource/image3.png" alt="image3.png"></li>
->>>>>>> 0811fbde4c51bfda1bb44ae35c618dcd94c20fa8
 								</ul>
 							</div>
 						</div>
