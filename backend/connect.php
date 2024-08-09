48ad643a40375cdb2b2c59e1f7866a51a7b68c0a
Ram Gore <gore64643@gmail.com>
2024-08-09 18:03:25 +0530

    email send to data bases

---
+++ backend/connect.php
@@ -0,0 +1,30 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'shirana');
+
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'u770425257_yogeshc');
+// define('DB_PASS', '~9Su&msJ82*z');
+// define('DB_NAME', 'u770425257_yogeshc');
+
+function connect()
+{
+  $connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+  if (mysqli_connect_errno()) { 
+    die("Failed to connect: " . mysqli_connect_error());
+  }
+
+  mysqli_set_charset($connect, "utf8");
+
+  return $connect;
+}
+
+?>
